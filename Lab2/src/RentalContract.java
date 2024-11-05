e579b6db7f9e940f62d962f1de103a61fc6ebd69
anhhh1801 <anh.doviet.cit21@eiu.edu.vn>
2024-11-05 08:37:33 +0700

    up

--- Lab2/src/RentalContract.java
+++ Lab2/src/RentalContract.java
@@ -1,4 +1,20 @@
 
 public class RentalContract {
-
+	int ContractID;
+	int PropertyID;
+	int TenantID;
+	double RentAmount;
+	boolean active;
+	
+	public RentalContract() {
+		super();
+	}
+	
+	public void CreatContract() {
+		this.active = true;
+	}
+	
+	public void TerminateContract() {
+		this.active = false;
+	}
 }
